6d22a385bf1c98209c773a7d3ea5d9e79b0c7dda
Andreas <balduran174@gmail.com>
2014-09-28 21:47:25 +0200

    menu

---
+++ Assets/Scripts/Menu/MenueItem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenueItem : MonoBehaviour
+{
+    public int MenuId = 1;
+
+    static int activemenuid = 0;
+
+    GameObject g;
+    void Start()
+    {
+        g = GameObject.Find("menutext");
+        g.renderer.enabled = false;
+    }
+    void LateUpdate()
+    {
+        if (MenuId != activemenuid)
+            return;
+        g.transform.position = transform.position + new Vector3(-7.7f, 7.6f, 0f);
+    }
+
+    void OnMouseEnter()
+    {
+        activemenuid = MenuId;
+        g.renderer.enabled = true;
+    }
+
+    void OnMouseExit()
+    {
+        //g.renderer.enabled = false;
+        //active = true;
+    }
+}
