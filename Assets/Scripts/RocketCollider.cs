a7744670d17b9d68e9d2fe4a8e6ea44406d2d9f5
Andreas <balduran174@gmail.com>
2014-09-28 23:43:41 +0200

    level1

---
+++ Assets/Scripts/RocketCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketCollider : MonoBehaviour {
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(123);
+        //if (col.gameObject.name == "Earth")
+        //{
+            this.gameObject.SetActive(false);
+            GameObject.Find("deadnotice").SetActive(true);
+        //}
+    }
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        Debug.Log(123);
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        Debug.Log(123);
+    }
+}
