7e71d276827db1b5a4c7b1e7fe4b097aa0b5bb0a
Andreas <balduran174@gmail.com>
2014-10-02 23:58:40 +0200

    aster

--- Assets/Scripts/AstroidSpawner.cs
+++ Assets/Scripts/AstroidSpawner.cs
@@ -1,17 +1,34 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.Linq;
 
 public class AstroidSpawner : MonoBehaviour
 {
+    public int radiusx = 100; 
+    public int radiusy = 100;
+
+    public int count = 100;
+
 
     void Start()
     {
-        GameObject g = new GameObject("AstroidT");
+        Sprite[] ss = UnityEditor.AssetDatabase.LoadAllAssetsAtPath("Assets/Sprites/Planets/Asteroids.png").OfType<Sprite>().ToArray();
+
+
+        GameObject g = new GameObject("At");
         SpriteRenderer s = g.AddComponent<SpriteRenderer>();
 
-        Sprite[] ss = UnityEditor.AssetDatabase.LoadAllAssetsAtPath("Assets/Sprites/Planets/Asteroids") as Sprite[];
-        s.sprite = ss[0];
+        for (int i = 0; i < count; i++)
+        {
+            GameObject gg = Instantiate(g, new Vector3(Mathf.Sin(2 * Mathf.PI / count * i) * radiusx, Mathf.Cos(2 * Mathf.PI / count * i) * radiusy), Quaternion.Euler(new Vector3(0, UnityEngine.Random.Range(0, Mathf.PI * 2), 0))) as GameObject;
+            s.sprite = ss[UnityEngine.Random.Range(0, 24)];
+
+            gg.transform.localScale = new Vector3(5, 5);
+
+            gg.transform.parent = this.transform;
+        }
+        GameObject.Destroy(g);
     }
 
 
