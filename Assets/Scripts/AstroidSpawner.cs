c2458756033b4d85a4540dc7edf18c440bb584f4
Andreas <balduran174@gmail.com>
2014-10-03 00:16:25 +0200

    asteroidenring angepasst

--- Assets/Scripts/AstroidSpawner.cs
+++ Assets/Scripts/AstroidSpawner.cs
@@ -21,10 +21,17 @@ public class AstroidSpawner : MonoBehaviour
 
         for (int i = 0; i < count; i++)
         {
-            GameObject gg = Instantiate(g, new Vector3(Mathf.Sin(2 * Mathf.PI / count * i) * radiusx, Mathf.Cos(2 * Mathf.PI / count * i) * radiusy), Quaternion.Euler(new Vector3(0, UnityEngine.Random.Range(0, Mathf.PI * 2), 0))) as GameObject;
+            float variance = 5;
+            GameObject gg = Instantiate(g,
+                new Vector3(
+                    Mathf.Sin(2 * Mathf.PI / count * i) * radiusx + UnityEngine.Random.Range(-variance, variance),
+                    Mathf.Cos(2 * Mathf.PI / count * i) * radiusy + UnityEngine.Random.Range(-variance, variance)),
+                Quaternion.Euler(new Vector3(0, UnityEngine.Random.Range(0, Mathf.PI * 2), 0))) as GameObject;
+
             s.sprite = ss[UnityEngine.Random.Range(0, 24)];
 
-            gg.transform.localScale = new Vector3(5, 5);
+            float f = UnityEngine.Random.Range(3, 7);
+            gg.transform.localScale = new Vector3(f, f);
 
             gg.transform.parent = this.transform;
         }
