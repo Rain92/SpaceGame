62a7dcbeef16d64d5842fb601d9fbae4bfd46898
Andreas <balduran174@gmail.com>
2014-10-02 23:02:32 +0200

    blub

---
+++ Assets/Scripts/AstroidSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class AstroidSpawner : MonoBehaviour
+{
+
+    void Start()
+    {
+        GameObject g = new GameObject("AstroidT");
+        SpriteRenderer s = g.AddComponent<SpriteRenderer>();
+
+        Sprite[] ss = UnityEditor.AssetDatabase.LoadAllAssetsAtPath("Assets/Sprites/Planets/Asteroids") as Sprite[];
+        s.sprite = ss[0];
+    }
+
+
+    void Update()
+    {
+
+    }
+}
